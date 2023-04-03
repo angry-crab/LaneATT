adb4b1547983217cdb7269bd0546c42879e30c24
Xinyu Wang <xinyu.wang@tier4.jp>
2023-04-03 16:14:20 +0900

    vis

--- inference.py
+++ inference.py
@@ -9,59 +9,64 @@ import cv2
 from lib.config import Config
 from lib.experiment import Experiment
 
-def draw_annotation(idx, label=None, pred=None, img=None):
-    # Get image if not provided
-    if img is None:
-        # print(self.annotations[idx]['path'])
-        img, label, _ = self.__getitem__(idx)
-        label = self.label_to_lanes(label)
-        img = img.permute(1, 2, 0).numpy()
-        if self.normalize:
-            img = img * np.array(IMAGENET_STD) + np.array(IMAGENET_MEAN)
-        img = (img * 255).astype(np.uint8)
-    else:
-        _, label, _ = self.__getitem__(idx)
-        label = self.label_to_lanes(label)
-    img = cv2.resize(img, (self.img_w, self.img_h))
-
+def draw_annotation(pred=None, img=None):
+    # _, label, _ = self.__getitem__(idx)
+    # label = self.label_to_lanes(label)
+    img_w = 460
+    img_h = 640
+    img = cv2.resize(img, (img_w, img_h))
     img_h, _, _ = img.shape
     # Pad image to visualize extrapolated predictions
     pad = 0
     if pad > 0:
-        img_pad = np.zeros((self.img_h + 2 * pad, self.img_w + 2 * pad, 3), dtype=np.uint8)
+        img_pad = np.zeros((img_h + 2 * pad, img_w + 2 * pad, 3), dtype=np.uint8)
         img_pad[pad:-pad, pad:-pad, :] = img
         img = img_pad
-    data = [(None, None, label)]
-    if pred is not None:
-        # print(len(pred), 'preds')
-        fp, fn, matches, accs = self.dataset.get_metrics(pred, idx)
-        # print('fp: {} | fn: {}'.format(fp, fn))
-        # print(len(matches), 'matches')
-        # print(matches, accs)
-        assert len(matches) == len(pred)
-        data.append((matches, accs, pred))
-    else:
-        fp = fn = None
-    for matches, accs, datum in data:
-        for i, l in enumerate(datum):
-            if matches is None:
-                color = GT_COLOR
-            elif matches[i]:
-                color = PRED_HIT_COLOR
-            else:
-                color = PRED_MISS_COLOR
-            points = l.points
-            points[:, 0] *= img.shape[1]
-            points[:, 1] *= img.shape[0]
-            points = points.round().astype(int)
-            points += pad
-            xs, ys = points[:, 0], points[:, 1]
-            for curr_p, next_p in zip(points[:-1], points[1:]):
-                img = cv2.line(img,
-                                tuple(curr_p),
-                                tuple(next_p),
-                                color=color,
-                                thickness=3 if matches is None else 3)
+    for i, l in enumerate(pred):
+        color = (0, 255, 0)
+        points = l.points
+        points[:, 0] *= img.shape[1]
+        points[:, 1] *= img.shape[0]
+        points = points.round().astype(int)
+        points += pad
+        for curr_p, next_p in zip(points[:-1], points[1:]):
+            img = cv2.line(img,
+                            tuple(curr_p),
+                            tuple(next_p),
+                            color=color,
+                            thickness=3)
+    # data = [(None, None, label)]
+    # if pred is not None:
+    #     # print(len(pred), 'preds')
+    #     fp, fn, matches, accs = self.dataset.get_metrics(pred, idx)
+    #     # print('fp: {} | fn: {}'.format(fp, fn))
+    #     # print(len(matches), 'matches')
+    #     # print(matches, accs)
+    #     assert len(matches) == len(pred)
+    #     data.append((matches, accs, pred))
+    # else:
+    #     fp = fn = None
+
+    # for matches, accs, datum in data:
+    #     for i, l in enumerate(datum):
+    #         if matches is None:
+    #             color = GT_COLOR
+    #         elif matches[i]:
+    #             color = PRED_HIT_COLOR
+    #         else:
+    #             color = PRED_MISS_COLOR
+    #         points = l.points
+    #         points[:, 0] *= img.shape[1]
+    #         points[:, 1] *= img.shape[0]
+    #         points = points.round().astype(int)
+    #         points += pad
+    #         xs, ys = points[:, 0], points[:, 1]
+    #         for curr_p, next_p in zip(points[:-1], points[1:]):
+    #             img = cv2.line(img,
+    #                             tuple(curr_p),
+    #                             tuple(next_p),
+    #                             color=color,
+    #                             thickness=3 if matches is None else 3)
             # if 'start_x' in l.metadata:
             #     start_x = l.metadata['start_x'] * img.shape[1]
             #     start_y = l.metadata['start_y'] * img.shape[0]
@@ -84,7 +89,7 @@ def draw_annotation(idx, label=None, pred=None, img=None):
             #                 fontFace=cv2.FONT_HERSHEY_COMPLEX,
             #                 fontScale=0.7,
             #                 color=(255, 0, 255))
-    return img, fp, fn
+    return img
 
 cfg_path = '/home/LaneATT/experiments/laneatt_r34_tusimple/config.yaml'
 cfg = Config(cfg_path)
@@ -111,4 +116,7 @@ with torch.no_grad():
 
     prediction = model.decode(obj_and_reg_output, as_lanes=True)
 
-    print(prediction[0])
+    img = (image.cpu().permute(1, 2, 0).numpy() * 255).astype(np.uint8)
+    img = draw_annotation(img=img, pred=prediction[0])
+    cv2.imshow('pred', img)
+    cv2.waitKey(0)
